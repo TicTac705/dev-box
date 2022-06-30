c525190d650dd9e7fb317d469321d2ecb8356a59
TicTac <geraskin707@gmail.com>
2022-06-30 16:55:21 +0300

    update

--- migrations/07-mongo.sh
+++ migrations/07-mongo.sh
@@ -10,6 +10,10 @@ echo "${GREEN}Setup mongo${NC}"
 echo "Upgrading mongo & make mongodb.so"
 sudo pecl upgrade mongodb
 
+sudo cp -f /home/vagrant/shop/devbox-shared/mongodb.conf /etc/mongodb.conf
+
+sudo systemctl restart mongodb
+
 sudo cp -f /home/vagrant/shop/devbox-shared/mongodb.ini /etc/php/7.4/mods-available/mongodb.ini
 
 sudo ln -sf /etc/php/7.4/mods-available/mongodb.ini /etc/php/7.4/fpm/conf.d/30-mongodb.ini
